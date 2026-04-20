ef0293468c7aae7ccf21d4f21ccfe3793b102da4
soaicasancho <chichchoehoa001@gmail.com>
2026-04-20 10:18:45 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.lambdacoresw.app1"
+    namespace = "com.dangiap.Metube"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.lambdacoresw.app1"
+        applicationId = "com.dangiap.Metube"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
